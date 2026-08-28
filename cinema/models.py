71b1ba5ee920311b372dfcc139d480941a0052f4
Ваше Имя <klypcontacts@gmail.com>
2026-08-28 16:21:14 +0200

    Fix unmatched parenthesis and clean up Movie.image definition

--- cinema/models.py
+++ cinema/models.py
@@ -51,7 +51,6 @@ class Movie(models.Model):
     genres = models.ManyToManyField(Genre, related_name="movies", blank=True)
     image = models.ImageField(null=True, upload_to=movie_image_file_path)
     )
-    )
 
     def __str__(self):
         return self.title

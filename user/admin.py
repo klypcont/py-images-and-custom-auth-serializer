c64bbfc08b2fe968011af30d15f5a1983d44a71b
Ваше Имя <klypcontacts@gmail.com>
2026-08-31 16:26:51 +0200

    Install Pillow and fix UserAdmin configuration for email-based user

--- user/admin.py
+++ user/admin.py
@@ -1,5 +1,36 @@
-from django.contrib import admin
+﻿from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from user.models import User
 
-admin.site.register(User, UserAdmin)
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    ordering = ("id",)
+    list_display = ("email", "first_name", "last_name", "is_staff")
+    search_fields = ("email", "first_name", "last_name")
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (("Personal info",), {"fields": ("first_name", "last_name")}),
+        (
+            ("Permissions",),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (("Important dates",), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2", "is_staff", "is_active"),
+            },
+        ),
+    )
